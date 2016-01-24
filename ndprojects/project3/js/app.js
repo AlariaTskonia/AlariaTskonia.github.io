ada1595af8adb17227fe7df04b71aa8c25b9cef7
AlariaTskonia <cr4725cs@gmail.com>
2016-01-24 10:50:48 -0800

    notes & bug velocity
    added my notes and location, and speed for bugs.  unsure about setting
    the direction...perhaps its always left to right?

--- ndprojects/project3/js/app.js
+++ ndprojects/project3/js/app.js
@@ -7,18 +7,24 @@ var Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 
-    this.x = -101;
-    this.y = Math.floor(Math.random() * 3) * 83 + 60;
-    this.speed = Math.floor(Math.random() * 400) + 50;
+    this.x = -101;  //Alaria: delcares x axis location for bug
+    this.y = Math.floor(Math.random() * 3) * 83 + 60; //Alaria: delcares y axis location for bug
+    this.speed = Math.floor(Math.random() * 400) + 50; //Alaria: delcares how fast the bugs will move
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+
+    this.x += this.speed * dt;  //Alaria: sets the speed using the dt parameter
+    if (this.x >505) {          //Alaria: if x axis is greater than 505 pixels
+        this.x = -101;          //Alaria: the bug will start off screen
+        this.y = Math.floor(Math.random() * 3) * 83 + 60;  //Alaria: at a random y axis
+        this.speed = Math.floor(Math.randomm() * 400) +50; //Alaria: at randomo speeds
+    };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
