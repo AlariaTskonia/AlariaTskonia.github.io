c1bff200068e3ec5f8ec05ec973ece61640f824d
AlariaTskonia <cr4725cs@gmail.com>
2016-01-24 11:12:48 -0800

    clean & move collide
    clean code/ spell check and moved collision code

--- ndprojects/project3/js/app.js
+++ ndprojects/project3/js/app.js
@@ -14,29 +14,34 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
+
 Enemy.prototype.update = function (dt) {
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 
     this.x += this.speed * dt;  //Alaria: sets the speed using the dt parameter
-    if (this.x >505) {          //Alaria: if x axis is greater than 505 pixels
+    if (this.x > 505) {          //Alaria: if x axis is greater than 505 pixels
         this.x = -101;          //Alaria: the bug will start off screen
         this.y = Math.floor(Math.random() * 3) * 83 + 60;  //Alaria: at a random y axis
-        this.speed = Math.floor(Math.randomm() * 400) +50; //Alaria: at randomo speeds
-    };
+        this.speed = Math.floor(Math.randomm() * 400) + 50; //Alaria: at random speeds
+    }
+
+    //Craigs collision code sans console log schtuff
+    var collision = Math.abs(player.x = this.x);            //Alaria: when the images share the same x axis
+    if (collision < 50.5 && this.y === player.y + 18.5) {   //Alaria: a collision is when both images have the same y axis
+        player.y = 373.5;                                   //Alaria: player y axis jumps to 373.5 pixels
+        player.loses += 1;                                  //Alaria: loss score gets +1
+        player.score = player.wins - player.loses;          //Alaria: total score is wins minus loses
+        document.getElementById("loses").innerHTML = player.loses; //Alaria: lose count
+        document.getElementById("total").innerHTML = player.score; //Alaria: total score
+    }
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
-    //Craigs collision code sans console log schtuff
-    var collision = Math.abs(player.x = this.x);            //Alaria: when the images share the same x axis
-    if (collision <50.5 && this.y === player.y + 18.5) {    //Alaria: a collision is when both images have the same y axis
-        player.y = 373.5;                                   //Alaria: player y axis jumps to 373.5 pixels
-        player.loses +=1;                                   //Alaria: loss score gets +1
-        player.score = player.wins - player.loses;          //Alaria: total score is wins minus loses
-    }
 };
 
 
